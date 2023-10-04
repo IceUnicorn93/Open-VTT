bf4120f33b9f7f2d51236e6b7cddd4248b59a935
Daniel <daniel.linke1993@googlemail.com>
2023-10-04 11:59:51 +0200

    Linux Test 8 | Script Reference Test

--- OpenVTT.Scripting/ScriptEngine.cs
+++ OpenVTT.Scripting/ScriptEngine.cs
@@ -359,7 +359,7 @@ Page.Controls.Add(tbDescription);";
 
             var list = new List<Assembly>();
 
-            try { list.Add(typeof(Settings.Settings).GetTypeInfo().Assembly); }
+            try { list.Add(typeof(OpenVTT.Settings.Settings).GetTypeInfo().Assembly); }
             catch { Logger.Log("Class: ScriptEngine | RunScript | Fail at Settings"); }
 
             try { list.Add(typeof(Session.Session).GetTypeInfo().Assembly); }
